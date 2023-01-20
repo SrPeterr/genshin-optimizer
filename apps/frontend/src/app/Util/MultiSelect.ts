57f60fba8fac15ab7628f572c0919bd62f1500ac
Van Nguyen <nguyentvan7@gmail.com>
2023-01-20 16:02:34 -0700

    Fix bug

--- apps/frontend/src/app/Util/MultiSelect.ts
+++ apps/frontend/src/app/Util/MultiSelect.ts
@@ -3,7 +3,7 @@ import { toggleArr } from "./Util"
 export function handleMultiSelect<T>(allKeys: T[]) {
   return (arr: T[], v: T): T[] => {
     const len = arr.length
-    if (len === allKeys.length || len === 0) return [v]
+    if (len === 0) return [v]
     if (len === 1 && arr[0] === v) return []
     return [...new Set(toggleArr(arr, v))]
   }
